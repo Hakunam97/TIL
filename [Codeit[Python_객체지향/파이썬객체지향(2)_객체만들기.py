a305b3bac0cfdbd6b332029a5b4d1f309de1dd68
Hakunam97 <haknam97@gmail.com>
2022-08-17 16:45:27 +0900

    class

--- [Codeit[Python_객체지향/파이썬객체지향(2)_객체만들기.py
+++ [Codeit[Python_객체지향/파이썬객체지향(2)_객체만들기.py
@@ -1,2 +1,24 @@
-## __str__메소드
+## __str__메소드 : 
+# 어떤 인스턴스를 print함수로 호출할 때 자동으로 실행
+# 인스턴스를 출력할 때 원하는 정보를 나오게 할 수 있음
 
+class User:
+    def __init__(self, name, email, pw):
+        # 유저 인스턴스의 모든 변수를 지정해주는 메소드 
+        self.name = name
+        self.email = email
+        self.pw = pw
+
+    def __str__(self):
+        return "사용자: {}, 이메일: {}, 비밀번호: ******".format(self.name, self.email)
+
+
+user1 = User("강영훈", "younghoon@codeit.kr", "123456")
+user2 = User("이윤수", "yoonsoo@codeit.kr", "1q2w3e4r")
+
+print(user1)    # 사용자: 강영훈, 이메일: younghoon@codeit.kr, 비밀번호: ******
+print(user2)    # 사용자: 이윤수, 이메일: yoonsoo@codeit.kr, 비밀번호: ******
+
+#------------------------------------------------------------------------------------------
+
+## 클래스 변수 1
