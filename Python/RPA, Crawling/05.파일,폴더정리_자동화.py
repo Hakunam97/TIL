5d0f8c6404bb2d5727b2b8fe878ff83026b2e2d2
Haknam Kim <haknam97@gmail.com>
2023-01-17 01:13:19 +0900

    .

--- Python/RPA, Crawling/05.파일,폴더정리_자동화.py
+++ Python/RPA, Crawling/05.파일,폴더정리_자동화.py
@@ -29,5 +29,54 @@ for file in files:
     if os.path.isfile(file):
         shutil.move(file, 'C:/Users/hakna/Downloads/이미지파일')
 
+
 # -----------------------------------------------------------------------
 
+# 강의 실습 풀이
+import os
+
+# 절대 경로 이용
+os.mkdir('C:/Users/hakna/Downloads/이미지파일')
+
+# 폴더가 없을 때만 만들기
+if not os.path.exists('C:/Users/hakna/Downloads/이미지파일'):
+    os.mkdir('C:/Users/hakna/Downloads/이미지파일')
+
+# 현재 폴더 내 모든 파일 출력 (리스트 형태로 출력)
+file_list = os.listdir('C:/Users/hakna/Downloads')
+print(file_list)
+
+# 반복문 통해 각 파일의 확장자 확인
+for file in file_list:
+    name, ext = os.path.splitext(file)  # unpacking : 각 원소들을 변수에 하나씩 담는 것
+
+    # tuple_a = os.path.splitext(file)
+    # name = tuple_a[0]
+    # ext = tuple_a[1]
+
+
+## 한번에 정리 ##
+
+# 정리할 확장자 리스트
+extension_list = ['.jpg', '.png', '.gif', '.jpeg']
+
+# 정리할 폴더
+target = "C:/Users/hakna/Downloads"
+
+# 만들 폴더
+destination = target + "/이미지파일"
+
+# 폴더가 없다면 만들기
+if not os.path.exists(destination):
+    os.mkdir(destination)
+
+# 현재 폴더 내 모든 파일 출력
+file_list = os.listdir(target)
+
+# 반복문을 통해 각 파일의 확장자를 확인
+for file in file_list:
+    name, ext = os.path.splitext(file)
+    if ext in extension_list:
+        # 파일 이동
+        source = os.path.join(target, file)
+        os.rename(source, os.path.join(destination, file))
