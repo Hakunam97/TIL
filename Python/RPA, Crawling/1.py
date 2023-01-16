12cc2602e36c87c623614e4e2041df864c070cac
Hakunam97 <haknam97@gmail.com>
2023-01-16 16:47:36 +0900

    .

---
+++ Python/RPA, Crawling/1.py
@@ -0,0 +1,54 @@
+# print("코딩으로 돈벌기", end=" ")
+# print("hello")
+
+# x = int(input("첫번째 숫자를 입력하세요 >>>"))
+# y = int(input("두번째 숫자를 입력하세요 >>>"))
+# print(x + y)
+
+# x = input("크롤링할 페이지 개수를 입력하세요 >>>")
+# print("크롤링할 페이지 개수는 " + x + "개 입니다.")
+
+# name = "김학남"
+# age = 20
+# print(f'{name} 나이는 {age}살 입니다.')
+
+# if age <20:
+#     print("미성년자입니다.")
+# else:
+#     print("성인입니다")
+
+
+# subscriber = int(input("현재 구독자 수를 입력하세요 >>> "))
+
+# if subscriber >= 1000:
+#     print("수익 창출이 가능합니다!")
+# else:
+#     print("수익 창출이 불가능합니다!")
+
+# study_time = int(input("공부시간을 입력하세요(시간) >>> "))
+
+# if study_time >= 10:
+#     print("휴대폰 잠금이 해제 됩니다.")
+# elif study_time >= 5:
+#     print("휴대폰을 30분간 사용가능 합니다.")
+# else:
+#     print("휴대폰 사용이 불가능 합니다")
+
+# c_list = ["1", "2", "3"]
+# c_list[0] = "2"
+# print(c_list)
+
+# c_list.append("4")
+# print(c_list)
+
+# del c_list[0]
+# print(c_list)
+
+# print(len(c_list))
+
+# person_info_list = [
+#     ["유재석", 51, "남"],
+#     ["노홍철", 44, "남"],
+#     ["하하", 44, "남"]
+# ]
+# print(person_info_list[0][0])
