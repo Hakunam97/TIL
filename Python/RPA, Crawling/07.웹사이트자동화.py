b3488ecbc76bfd05d2052fb9782d5a9a2187be0f
Haknam Kim <haknam97@gmail.com>
2023-01-27 00:18:33 +0900

    .

--- Python/RPA, Crawling/07.웹사이트자동화.py
+++ Python/RPA, Crawling/07.웹사이트자동화.py
@@ -12,6 +12,10 @@ from selenium.webdriver.common.by import By
 # 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
 
+import time
+import pyautogui
+import pyperclip
+
 # 브라우저 꺼짐 방지
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -32,17 +36,26 @@ driver.get("https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww
 #--------------------------------------------------------------------------------------------------------------
 ## 네이버 자동 로그인
 
-# 네이버 로그인 창에서 F12 -> 구글 개발자 도구 -> Ctri+Shift+C 돋보기 버튼 
+# 네이버 로그인 창에서 F12 -> 구글 개발자 도구 -> Ctrl+Shift+C 돋보기 버튼 
 # -> '아이디' 부분 클릭 -> 해당하는 Tag를 찾음 (HTML) -> copy selector (비밀번호 과정도 같음)
 
 # 아이디 입력창
 id = driver.find_element(By.CSS_SELECTOR, "#id") # #id -> 복사했던 Tag를 붙여넣은 것, # 해당 CSS selector에 맞는 Tag를 자동으로 찾아줌
 id.click()  # 클릭
-id.send_keys("haknam0216")  # 키보드 입력
+pyperclip.copy("haknam0216")  # 키보드 입력 (너무 빠르게 입력하면 네이버에서 봇으로 인식하기 때문에 천천히 입력)
+pyautogui.hotkey("ctrl", "v")
+time.sleep(2)
 
 # 비밀번호 입력창
 pw = driver.find_element(By.CSS_SELECTOR, "#pw") 
 pw.click()  
-pw.send_keys("akstk0216~")  
+pyperclip.copy("akstk0216~")  
+pyautogui.hotkey("ctrl", "v")
+time.sleep(2)
 
-# 15:58부터 시청!!!
+# 로그인 버튼 클릭
+login_btn = driver.find_element(By.CSS_SELECTOR, "#log\.login")
+login_btn.click()
+
+login_btn2 = driver.find_element(By.CSS_SELECTOR, "#new\.save")
+login_btn2.click()
